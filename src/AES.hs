050f5e45db8491cbf9bc52d34d59c1bae433703b
Thomas Bidne <tbidne@gmail.com>
2018-05-29 18:38:13 -0400

    add aes block decrypt

--- src/AES.hs
+++ src/AES.hs
@@ -1,18 +1,22 @@
 module AES
 ( keygenIO
 , encryptIO
+, decryptIO
 , keygen
 , encrypt
+, decrypt
 )
 where
 
-import qualified System.Random as R
-import qualified Data.ByteString.Lazy as BS
-import qualified Data.Bits as B
-import qualified Data.List as L
-import qualified Data.Word as W
+import qualified System.Random as R (newStdGen, RandomGen, randomR)
+import qualified Data.ByteString.Lazy as BS (ByteString, pack, readFile, unpack, writeFile)
+import qualified Data.Bits as B (Bits, shiftL, shiftR, xor, (.&.))
+import qualified Data.List as L (concat, map, transpose)
+import qualified Data.Word as W (Word8)
 
 -- Based on FIPS 197: https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.197.pdf
+-- TODO:
+-- modes (ECB, CBC)
 
 ------------------
 -- IO Functions --
@@ -57,23 +61,25 @@ keygen g size = case size of
 -- Encrypts message m with key k
 encrypt :: BS.ByteString -> BS.ByteString -> BS.ByteString
 encrypt k m = stateToByteStr encrypted
-  where key = keyInit k
-        roundKeys = keySchedule key
-        state = stateInit m
-        rounds = length key + 6
-        encrypted = encryptInit rounds roundKeys state
+  where (maxRound, roundKeys, state) = setupForTransform k m
+        encrypted = encryptInit maxRound roundKeys state
 
+-- Decrypts message m with key k
 decrypt :: BS.ByteString -> BS.ByteString -> BS.ByteString
 decrypt k m = stateToByteStr decrypted
-  where key = keyInit k
-        roundKeys = keySchedule key
-        state = stateInit m
-        rounds = (length key ` div` 4) + 6
-        decrypted = decryptInit rounds roundKeys state
+  where (maxRound, roundKeys, state) = setupForTransform k m
+        decrypted = decryptInit maxRound roundKeys state
 
 --------------------
 -- Init Functions --
 --------------------
+
+setupForTransform :: BS.ByteString -> BS.ByteString -> (Int, [[W.Word8]], [[W.Word8]])
+setupForTransform k m = (maxRound, roundKeys, state)
+  where key = keyInit k
+        roundKeys = keySchedule key
+        maxRound = length key + 6
+        state = stateInit m
 
 -- Turns ByteString representing key into matrix e.g. 128 bit keys have form
 -- | b1  b2  b3  b4  |
@@ -139,46 +145,57 @@ shiftRows (x:xs) = x : shiftRows shifted
 mixColumns :: [[W.Word8]] -> [[W.Word8]]
 mixColumns state = L.transpose mixed
   where transposed = L.transpose state
-        col0 = affinetransform $ head transposed
-        col1 = affinetransform $ transposed !! 1
-        col2 = affinetransform $ transposed !! 2
-        col3 = affinetransform $ transposed !! 3
+        col0 = affineTransform $ head transposed
+        col1 = affineTransform $ transposed !! 1
+        col2 = affineTransform $ transposed !! 2
+        col3 = affineTransform $ transposed !! 3
         mixed = [col0] ++ [col1] ++ [col2] ++ [col3]
-
--- xors the state with the current round key
-addRoundKey :: Int -> [[W.Word8]] -> [[W.Word8]] -> [[W.Word8]]
-addRoundKey round roundKeys state = xorMatrix state roundKey []
-  where lowIdx = 4 * round
-        highIdx = 4 * round + 3
-        roundKey = L.transpose $ drop lowIdx $ take (highIdx + 1) roundKeys
 
 --------------------------------
 -- Rijndael Decrypt Functions --
 --------------------------------
 
+-- Round Nr: starts the decryption, adds the last round key
 decryptInit :: Int -> [[W.Word8]] -> [[W.Word8]] -> [[W.Word8]]
-decryptInit rounds key state = decryptRound rounds key state
+decryptInit maxRound roundKeys state = decryptRound (maxRound-1) roundKeys state'
+  where state' = addRoundKey maxRound roundKeys state
 
+-- Rounds Nr-1 to 1
 decryptRound :: Int -> [[W.Word8]] -> [[W.Word8]] -> [[W.Word8]]
-decryptRound 0 key state = decryptFinalize key state
-decryptRound rounds key state =
-  let modify = invMixColumns . invShiftRows . invSubBytes
-  in decryptRound (rounds-1) key $ invAddRoundKey key $ modify state
+decryptRound 0 roundKeys state = decryptFinalize roundKeys state
+decryptRound round roundKeys state = decryptRound (round-1) roundKeys state'
+  where transform = invSubBytes . invShiftRows
+        state' = invMixColumns $ addRoundKey round roundKeys $ transform state
 
+-- Round 0: final transformation
 decryptFinalize :: [[W.Word8]] -> [[W.Word8]] -> [[W.Word8]]
-decryptFinalize key state = state
+decryptFinalize roundKeys state = state'
+  where transform = invSubBytes . invShiftRows
+        state' = addRoundKey 0 roundKeys $ transform state
 
+-- Non-linear transformation mapping state to invSbox(state)
 invSubBytes :: [[W.Word8]] -> [[W.Word8]]
-invSubBytes state = state
+invSubBytes = L.map invSubWord
 
+-- Shifts rows according to
+-- | b1  b2  b3  b4  |      | b1  b2  b3  b4  |
+-- | b6  b7  b8  b5  |  ->  | b5  b6  b7  b8  |
+-- | b10 b11 b8  b9  |      | b9  b10 b11 b12 |
+-- | b15 b12 b13 b14 |      | b13 b14 b15 b16 |
 invShiftRows :: [[W.Word8]] -> [[W.Word8]]
-invShiftRows state = state
+invShiftRows [] = []
+invShiftRows l = undoShifts l !! 3
+  where undoShifts = iterate rotate . reverse . shiftRows. reverse . rotate
 
+-- Mixes columns based on below inverse affine transformation
 invMixColumns :: [[W.Word8]] -> [[W.Word8]]
-invMixColumns state = state
-
-invAddRoundKey :: [[W.Word8]] -> [[W.Word8]] -> [[W.Word8]]
-invAddRoundKey key state = state
+invMixColumns state = L.transpose mixed
+  where transposed = L.transpose state
+        col0 = invAffineTransform $ head transposed
+        col1 = invAffineTransform $ transposed !! 1
+        col2 = invAffineTransform $ transposed !! 2
+        col3 = invAffineTransform $ transposed !! 3
+        mixed = [col0] ++ [col1] ++ [col2] ++ [col3]
 
 -------------------------------------
 -- Rijndael Key Schedule Functions --
@@ -218,6 +235,10 @@ coreTransform i nk word
 subWord :: [W.Word8] -> [W.Word8]
 subWord = L.map subByte
 
+-- Substitutes an entire vector based on the inverse sbox
+invSubWord :: [W.Word8] -> [W.Word8]
+invSubWord = L.map invSubByte
+
 -- Rotates a list by 1, e.g., [1,2,3] -> [2,3,1]
 rotate :: [a] -> [a]
 rotate (x:xs) = xs ++ [x]
@@ -239,9 +260,23 @@ rcon 10 = 54
 -- Other Rijndael Functions --
 ------------------------------
 
+-- xors the state with the current round key
+addRoundKey :: Int -> [[W.Word8]] -> [[W.Word8]] -> [[W.Word8]]
+addRoundKey round roundKeys state = xorMatrix state roundKey []
+  where lowIdx = 4 * round
+        highIdx = 4 * round + 3
+        roundKey = L.transpose $ drop lowIdx $ take (highIdx + 1) roundKeys
+
 -- Returns a byte based on Rijndael sbox.
 subByte :: W.Word8 -> W.Word8
-subByte a = (sbox !! row) !! col
+subByte = subByteHelper sbox
+
+-- Returns a byte based on Rijndael inverse sbox.
+invSubByte :: W.Word8 -> W.Word8
+invSubByte = subByteHelper invSbox
+
+subByteHelper :: [[W.Word8]] -> W.Word8 -> W.Word8
+subByteHelper matrix a = (matrix !! row) !! col
   where row = fromIntegral $ B.shiftR a 4 ::Int -- left most half byte
         col = fromIntegral a B..&. 15 ::Int -- right most half byte
 
@@ -265,13 +300,30 @@ sbox = [row0,row1,row2,row3,row4,row5,row6,row7,row8,row9,row10,row11,row12,row1
         row15 = [140, 161, 137, 13, 191, 230, 66, 104, 65, 153, 45, 15, 176, 84, 187, 22]
 
 invSbox :: [[W.Word8]]
-invSbox = L.transpose sbox
+invSbox = [row0,row1,row2,row3,row4,row5,row6,row7,row8,row9,row10,row11,row12,row13,row14,row15]
+  where row0 = [82, 009, 106, 213, 048, 054, 165, 056, 191, 064, 163, 158, 129, 243, 215, 251]
+        row1 = [124, 227, 057, 130, 155, 047, 255, 135, 52, 142, 067, 68, 196, 222, 233, 203]
+        row2 = [84, 123, 148, 50, 166, 194, 35, 61, 238, 76, 149, 011, 66, 250, 195, 78]
+        row3 = [8, 46, 161, 102, 40, 217, 36, 178, 118, 91, 162, 73, 109, 139, 209, 37]
+        row4 = [114, 248, 246, 100, 134, 104, 152, 22, 212, 164, 92, 204, 93, 101, 182, 146]
+        row5 = [108, 112, 72, 80, 253, 237, 185, 218, 94, 21, 70, 87, 167, 141, 157, 132]
+        row6 = [144, 216, 171, 0, 140, 188, 211, 10, 247, 228, 88, 5, 184, 179, 69, 6]
+        row7 = [208, 44, 30, 143, 202, 63, 15, 2, 193, 175, 189, 3, 1, 19, 138, 107]
+        row8 = [58, 145, 17, 65, 79, 103, 220, 234, 151, 242, 207, 206, 240, 180, 230, 115]
+        row9 = [150, 172, 116, 34, 231, 173, 53, 133, 226, 249, 55, 232, 28, 117, 223, 110]
+        row10 = [71, 241, 26, 113, 29, 41, 197, 137, 111, 183, 98, 14, 170, 24, 190, 027]
+        row11 = [252, 86, 62, 75, 198, 210, 121, 32, 154, 219, 192, 254, 120, 205, 90, 244]
+        row12 = [31, 221, 168, 51, 136, 7, 199, 49, 177, 18, 16, 89, 39, 128, 236, 095]
+        row13 = [96, 81, 127, 169, 25, 181, 74, 13, 45, 229, 122, 159, 147, 201, 156, 239]
+        row14 = [160, 224, 59, 77, 174, 42, 245, 176, 200, 235, 187, 60, 131, 83, 153, 97]
+        row15 = [23, 43, 4, 126, 186, 119, 214, 38, 225, 105, 20, 99, 85, 33, 12, 125]
 
 ----------------------
 -- Helper Functions --
 ----------------------
 
--- We only need to implement multiplication by 2 and 3 in GF(2^8).
+-- We only need to implement multiplication by 2 and 3 in GF(2^8) for encryption,
+-- 9, 11, 13, and 14 for decryption.
 -- Multiplication by 2 is equivalent to bit shifting by one and adding (xor)
 -- 0x1b (27) if the high bit was set.
 -- 3 x b = (2 xor 1) b = (2 x b) xor b
@@ -281,7 +333,11 @@ fieldMult 2 b
   | otherwise  = shifted
   where shifted = b `B.shiftL` 1
         highBitSet = b B..&. 128 == 128
-fieldMult 3 b = b `B.xor` fieldMult 2 b -- 3 x b = (2 x b) xor b
+fieldMult 3 b = B.xor b (fieldMult 2 b) -- 3 x b = (2 x b) xor b
+fieldMult 9 b =  B.xor b (fieldMult 2 (fieldMult 2 (fieldMult 2 b)))
+fieldMult 11 b = B.xor b (fieldMult 2 (B.xor b (fieldMult 2 (fieldMult 2 b))))
+fieldMult 13 b = B.xor b (fieldMult 2 (fieldMult 2 (B.xor b (fieldMult 2 b))))
+fieldMult 14 b = fieldMult 2 (B.xor b (fieldMult 2 (B.xor b (fieldMult 2 b))))
 
 -- Performs an affine transformation on the param vector based on the matrix
 -- | 2 3 1 1 |
@@ -289,24 +345,49 @@ fieldMult 3 b = b `B.xor` fieldMult 2 b -- 3 x b = (2 x b) xor b
 -- | 1 1 2 3 |
 -- | 3 1 1 2 |
 -- Addition is xor and multiplication is in GF(2^8).
-affinetransform :: [W.Word8] -> [W.Word8]
-affinetransform vector = [bOne] ++ [bTwo] ++ [bThree] ++ [bFour]
-  where bOne = fieldMult 2 (head vector) `B.xor`
-               fieldMult 3 (vector !! 1) `B.xor`
-               vector !! 2 `B.xor`
-               vector !! 3
-        bTwo = head vector `B.xor`
-               fieldMult 2 (vector !! 1) `B.xor`
-               fieldMult 3 (vector !! 2) `B.xor`
-               vector !! 3
+affineTransform :: [W.Word8] -> [W.Word8]
+affineTransform vector = [bOne] ++ [bTwo] ++ [bThree] ++ [bFour]
+  where bOne =   fieldMult 2 (head vector) `B.xor`
+                 fieldMult 3 (vector !! 1) `B.xor`
+                 vector !! 2 `B.xor`
+                 vector !! 3
+        bTwo =   head vector `B.xor`
+                 fieldMult 2 (vector !! 1) `B.xor`
+                 fieldMult 3 (vector !! 2) `B.xor`
+                 vector !! 3
         bThree = head vector `B.xor`
                  vector !! 1 `B.xor`
                  fieldMult 2 (vector !! 2) `B.xor`
                  fieldMult 3 (vector !! 3)
-        bFour = fieldMult 3 (head vector) `B.xor`
-                vector !! 1 `B.xor`
-                vector !! 2 `B.xor`
-                fieldMult 2 (vector !! 3)
+        bFour =  fieldMult 3 (head vector) `B.xor`
+                 vector !! 1 `B.xor`
+                 vector !! 2 `B.xor`
+                 fieldMult 2 (vector !! 3)
+
+-- Performs an affine transformation on the param vector based on the matrix
+-- | 14 11 13  9 |
+-- | 9  14 11 |3 |
+-- | 13 9  14 11 |
+-- | 11 13 9  14 |
+-- Addition is xor and multiplication is in GF(2^8).
+invAffineTransform :: [W.Word8] -> [W.Word8]
+invAffineTransform vector = [bOne] ++ [bTwo] ++ [bThree] ++ [bFour]
+  where bOne =   fieldMult 14 (head vector) `B.xor`
+                 fieldMult 11 (vector !! 1) `B.xor`
+                 fieldMult 13 (vector !! 2) `B.xor`
+                 fieldMult 9  (vector !! 3)
+        bTwo =   fieldMult 9  (head vector) `B.xor`
+                 fieldMult 14 (vector !! 1) `B.xor`
+                 fieldMult 11 (vector !! 2) `B.xor`
+                 fieldMult 13 (vector !! 3)
+        bThree = fieldMult 13 (head vector) `B.xor`
+                 fieldMult 9  (vector !! 1) `B.xor`
+                 fieldMult 14 (vector !! 2) `B.xor`
+                 fieldMult 11 (vector !! 3)
+        bFour =  fieldMult 11 (head vector) `B.xor`
+                 fieldMult 13 (vector !! 1) `B.xor`
+                 fieldMult 9  (vector !! 2) `B.xor`
+                 fieldMult 14 (vector !! 3)
 
 -- Xors every elemnt in list with param.
 xorByte :: B.Bits a => [a] -> a -> [a]
